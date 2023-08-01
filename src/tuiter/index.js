a00dcd2c2dd1c7e4ae8395a8848b86f2cfc613e1
deleot <deleo.t@northeastern.edu>
2023-08-01 12:04:06 -0400

    a4

--- src/tuiter/index.js
+++ src/tuiter/index.js
@@ -1,35 +1,42 @@
-import { Routes, Route } from "react-router";
+import { Routes, Route, Navigate} from "react-router";
 import Nav from "../nav";
 import NavigationSidebar from "./navigation-sidebar";
-import HomeScreen from "./home-screen";
+import HomeScreen from "./home";
 import ExploreScreen from "./explore-screen";
 import BookmarksScreen from "./bookmarks-screen";
 import ProfileScreen from "./profile-screen";
 import WhoToFollowList from "./who-to-follow-list";
+import whoReducer from "./reducers/who-reducer";
+import tuitsReducer from "./reducers/tuits-reducer";
+import { configureStore } from '@reduxjs/toolkit';
+import { Provider } from "react-redux";
+const store = configureStore(
+    { reducer: { who: whoReducer, tuits: tuitsReducer } });
 
 
 
 function Tuiter() {
     return (
-        <div>
+        <Provider store={store}>
             <Nav />
             <div className="row">
-                <div className="col-2">
+                <div className="col-3 col-lg-2">
                     <NavigationSidebar />
                 </div>
-                <div className="col-7">
+                <div className="col-9 col-lg-7">
                     <Routes>
+                        <Route path="/" element={<Navigate to="/tuiter/home"/>} />
                         <Route path="/home" element={<HomeScreen />} />
-                        <Route path="/explore" element={<ExploreScreen/>} />
+                        <Route path="/explore" element={<ExploreScreen />} />
                         <Route path="/bookmarks" element={<BookmarksScreen />} />
                         <Route path="/profile" element={<ProfileScreen />} />
                     </Routes>
                 </div>
-                <div className="col-3">
-                    <WhoToFollowList/>
+                <div className="col-3 d-none d-lg-block">
+                    <WhoToFollowList />
                 </div>
             </div>
-        </div>
+        </Provider>
     );
 }
 export default Tuiter;
