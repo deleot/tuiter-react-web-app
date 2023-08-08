da169bfec1bf65b830def90df4818ffeebc018b0
deleot <deleo.t@northeastern.edu>
2023-08-08 18:33:26 -0400

    a5 wip

--- src/tuiter/reducers/tuits-reducer.js
+++ src/tuiter/reducers/tuits-reducer.js
@@ -1,44 +1,87 @@
 import { createSlice } from "@reduxjs/toolkit";
-import tuits from "./tuits.json";
-import { findTuitsThunk } from "../services/tuits-thunks";
+import tuits from './tuits.json';
+import { updateTuitThunk, createTuitThunk, deleteTuitThunk, findTuitsThunk } from "../services/tuits-thunks";
 const initialState = {
-  tuits: [],
-  loading: false,
-};
+    tuits: [],
+    loading: false
+}
 
 const currentUser = {
-  userName: "NASA",
-  handle: "@nasa",
-  image: "nasa.png",
+    "userName": "NASA",
+    "handle": "@nasa",
+    "image": "nasa.png",
 };
 
 const templateTuit = {
-  ...currentUser,
-  topic: "Space",
-  time: "2h",
-  liked: false,
-  replies: 0,
-  retuits: 0,
-  likes: 0,
-};
+    ...currentUser,
+    "topic": "Space",
+    "time": "2h",
+    "liked": false,
+    "replies": 0,
+    "retuits": 0,
+    "likes": 0,
+}
+
 
 const tuitsSlice = createSlice({
-  name: "tuits",
-  initialState,
-  extraReducers: {
-    [findTuitsThunk.pending]: (state) => {
-      state.loading = true;
-      state.tuits = [];
+    name: 'tuits',
+    // initialState: { tuits: tuits },
+    initialState,
+    extraReducers: {
+        [updateTuitThunk.fulfilled]:
+            (state, { payload }) => {
+                state.loading = false
+                const tuitNdx = state.tuits.findIndex((t) => t._id === payload._id)
+                state.tuits[tuitNdx] = { ...state.tuits[tuitNdx], ...payload }
+            },
+        [createTuitThunk.fulfilled]:
+            (state, { payload }) => {
+                state.loading = false
+                state.tuits.push({ ...templateTuit, payload })
+            },
+        [deleteTuitThunk.fulfilled]:
+            (state, { payload }) => {
+                state.loading = false
+                state.tuits = state.tuits.filter(t => t._id !== payload)
+            },
+        [findTuitsThunk.pending]:
+            (state) => {
+                state.loading = true
+                state.tuits = []
+            },
+        [findTuitsThunk.fulfilled]:
+            (state, { payload }) => {
+                state.loading = false
+                state.tuits = payload
+            },
+        [findTuitsThunk.rejected]:
+            (state, action) => {
+                state.loading = false
+                state.error = action.error
+            }
     },
-    [findTuitsThunk.fulfilled]: (state, { payload }) => {
-      state.loading = false;
-      state.tuits = payload;
-    },
-    [findTuitsThunk.rejected]: (state, action) => {
-      state.loading = false;
-      state.error = action.error;
-    },
-  },
 
-  reducers: { }
+    reducers: {
+        tuitLikedToggle(state, action) {
+            const tuit = state.tuits.find((tuit) => tuit._id === action.payload._id)
+            tuit.liked = !tuit.liked
+            tuit.likes = tuit.liked ? tuit.likes + 1 : tuit.likes - 1
+        },
+        createTuit(state, action) {
+            state.tuits.unshift({
+                ...action.payload,
+                ...templateTuit,
+                _id: (new Date()).getTime(),
+            })
+        },
+        deleteTuit(state, action) {
+            const index = state.tuits
+                .findIndex(tuit =>
+                    tuit._id === action.payload);
+            state.tuits.splice(index, 1);
+        },
+    }
+
 });
+export const { tuitLikedToggle, createTuit, deleteTuit } = tuitsSlice.actions
+export default tuitsSlice.reducer;
--- tuiter-node-server-app/app.js
+++ tuiter-node-server-app/app.js
@@ -1,12 +1,31 @@
-import express from "express";
+import express from "express-session";
 import cors from 'cors'
 import HelloController from "./controllers/hello-controller.js";
 import UserController from "./users/users-controller.js";
 import TuitsController from "./controllers/tuits/tuits-controller.js";
+import AuthController from "./users/auth-controller.js";
 const app = express();
+app.use(
+ cors({
+   credentials: true,
+   origin: "http://localhost:3000",
+ })
+);
+const sessionOptions = {
+    secret: "any string",
+    resave: false,
+    saveUninitialized: false,
+  };
+  app.use(
+    session(sessionOptions)
+  );
+  
+app.use(express.json());
+const port = process.env.PORT || 4000;
 app.use(express.json());
 HelloController(app);
 TuitsController(app);
 UserController(app);
 app.use(cors())
 app.listen(4000);
+AuthController(app);
