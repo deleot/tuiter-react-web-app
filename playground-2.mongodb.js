3abec9e1802191cf9eb244562cd52fcd8f10896c
deleot <deleo.t@northeastern.edu>
2023-08-15 20:53:39 -0400

    a6 wip

--- playground-2.mongodb.js
+++
@@ -1,8 +0,0 @@
-use('tutier-su2-23');
-db.users.drop();
-
-db.users.insertMany([
-    {'_id': 1, 'username': 'alice', 'password'},
-    {'_id': 1, 'username': 'bob', 'password'},
-    {'_id': 1, 'username': 'charlie', 'password'},
-]);
